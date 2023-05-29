f69403d30a4530c07eb8e456954bed806d3827e5
lzb <liuzibin@timingbar.com>
2023-05-29 18:51:42 +0800

    fix(移除): 包install

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -1,3 +1,8 @@
+#!/usr/bin/env node
+
+import fs from 'node:fs';
+import path from 'node:path';
+
 //1. 判断node的版本
 // 限制的版本 node 14.18.0
 const limitNodeVersion = '14.18.0';
@@ -21,8 +26,30 @@ for (let i = 0, len = versionList.length; i < len; i++) {
   }
 }
 
+/**
+ * @description: 移除文件夹
+ * @param {*} dir
+ * @return {*}
+ */
+function removeDir(dir) {
+  let files = fs.readdirSync(dir);
+  for (var i = 0; i < files.length; i++) {
+    let newPath = path.join(dir, files[i]);
+    let stat = fs.statSync(newPath);
+    if (stat.isDirectory()) {
+      //如果是文件夹就递归下去
+      removeDir(newPath);
+    } else {
+      //删除文件
+      fs.unlinkSync(newPath);
+    }
+  }
+  fs.rmdirSync(dir); //如果文件夹是空的，就将自己删除掉
+}
+
 // 2. 判断pnpm
 if (!/pnpm/.test(process.env.npm_execpath || '')) {
   console.log(`\n【此处警告⚠️】😂😂😂😂请使用pnpm包管理安装！！！！！！！\n`);
+  removeDir('node_modules');
   process.exit(1);
 }
